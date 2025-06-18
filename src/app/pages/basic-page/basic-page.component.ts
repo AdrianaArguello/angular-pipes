678a799bbd57b46aca07c6246cd8e34550bbfaa3
Adriana Argüello <70598371+AdrianaArguello@users.noreply.github.com>
2025-06-18 10:58:23 -0400

    Implemented basic and numeric pipes screens as distinct views

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,14 +1,18 @@
-import { LowerCasePipe } from '@angular/common';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe],
+  imports: [
+    LowerCasePipe,
+    UpperCasePipe,
+    TitleCasePipe
+  ],
   templateUrl: './basic-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class BasicPageComponent {
   nameLower = signal('adriana');
   nameUpper = signal('ADRIANA');
-  fullName = signal('aDrIaNa');
+  fullName = signal('aDrIaNa aRgUeLLo');
 }
