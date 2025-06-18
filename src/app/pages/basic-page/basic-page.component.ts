4c7b8c21c4f685e084f57992d3aa1e6127356257
Adriana Argüello <70598371+AdrianaArguello@users.noreply.github.com>
2025-06-18 13:31:10 -0400

    Added UI text translations

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,18 +1,36 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, effect, inject, LOCALE_ID, signal } from '@angular/core';
+import { AvailableLocale, LocaleService } from '../../services/locale.service';
 
 @Component({
   selector: 'app-basic-page',
   imports: [
     LowerCasePipe,
     UpperCasePipe,
-    TitleCasePipe
+    TitleCasePipe,
+    DatePipe
   ],
   templateUrl: './basic-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class BasicPageComponent {
+  localeService = inject(LocaleService);
+  currentLocale = signal(inject(LOCALE_ID));
+
   nameLower = signal('adriana');
   nameUpper = signal('ADRIANA');
   fullName = signal('aDrIaNa aRgUeLLo');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect(() => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  });
+
+  changeLocale(locale: AvailableLocale){
+    this.localeService.changeLocale(locale);
+  }
 }
