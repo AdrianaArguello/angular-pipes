fee5df9eda14614156e1e19464734a9e43bdb772
Adriana Argüello <70598371+AdrianaArguello@users.noreply.github.com>
2025-07-02 09:01:10 -0400

    update custom pipe

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { heroes } from '../../data.heroes';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [ToggleCasePipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
+  name = signal('Adriana Arguello');
+  upperCase = signal(true);
+  heroes = signal(heroes)
 
+  toggleCase() {
+    this.upperCase.update(upper => !upper);
+  }
 }
